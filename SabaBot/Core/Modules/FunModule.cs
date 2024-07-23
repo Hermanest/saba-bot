75d1284a6521631d81d84488f40081f62fe71e76
Hermanest <nicedevelooper@gmail.com>
2024-07-23 22:07:23 +0300

    Added commands: gay, love and liar.

---
+++ SabaBot/Core/Modules/FunModule.cs
@@ -0,0 +1,44 @@
+﻿using Discord;
+using Discord.Interactions;
+using JetBrains.Annotations;
+
+namespace SabaBot.Modules;
+
+public class FunModule : LocalizedInteractionModuleBase {
+    [SlashCommand("gay", "Shows how much you are gay"), UsedImplicitly]
+    private async Task HandleGayCommand(IUser user) {
+        await DeferAsync();
+        var message = await GetLocalizedKey("GayMessage");
+        var percent = GetRandomNumber();
+        await DeleteOriginalResponseAsync();
+        await ReplyAsync(string.Format(message, user.Mention, percent));
+    }
+    
+    [SlashCommand("love", "Shows how much you love the specified user."), UsedImplicitly]
+    private async Task HandleLoveCommand(IUser user) {
+        await DeferAsync();
+        var message = await GetLocalizedKey("LoveMessage");
+        var percent = GetRandomNumber();
+        await DeleteOriginalResponseAsync();
+        await ReplyAsync(string.Format(message, Context.User.Mention, user.Mention, percent));
+    }
+    
+    [SlashCommand("liar", "Shows the user is liar or not"), UsedImplicitly]
+    private async Task HandleLiarCommand(IUser user) {
+        await DeferAsync();
+        var useAlt = GetRandomBool(1);
+        var liar = GetRandomBool();
+        var key = liar ? useAlt ? "AltLiarMessage" : "LiarMessage" : "NotLiarMessage";
+        var message = await GetLocalizedKey(key);
+        await DeleteOriginalResponseAsync();
+        await ReplyAsync(string.Format(message, user.Mention));
+    }
+    
+    private static bool GetRandomBool(int chance = 50) {
+        return GetRandomNumber() > chance;
+    }
+    
+    private static int GetRandomNumber() {
+        return new Random().Next(0, 101);
+    }
+}
